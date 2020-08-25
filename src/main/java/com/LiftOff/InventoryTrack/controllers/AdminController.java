8dac43f2be9541094910c781d5fe943753becdb4
Ethan Bell <ethanbell@ethans-mbp.attlocal.net>
2020-08-25 11:17:39 -0500

    configured storefronts html and controllers for storefronts

--- src/main/java/com/LiftOff/InventoryTrack/controllers/AdminController.java
+++ src/main/java/com/LiftOff/InventoryTrack/controllers/AdminController.java
@@ -3,6 +3,7 @@ package com.LiftOff.InventoryTrack.controllers;
 import com.LiftOff.InventoryTrack.data.ProductRepository;
 import com.LiftOff.InventoryTrack.data.StorefrontRepository;
 import com.LiftOff.InventoryTrack.models.Product;
+import com.LiftOff.InventoryTrack.models.Storefront;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 public class AdminController {
@@ -37,7 +40,22 @@ public class AdminController {
     {
         productRepository.save(new Product(name, description, price, quantity));
 
-        return "redirect:../";
+        return "admin/editProducts.html";
+    }
+
+    @GetMapping(value = "editStorefronts")
+    public String displayStorefronts(Model model)
+    {
+        model.addAttribute("title", "All Storefronts");
+        model.addAttribute("storefronts", storefrontRepository.findAll());
+        return "admin/editStorefronts.html";
+    }
+    @PostMapping(value = "editStorefronts")
+    public String processAddStorefrontForm(@RequestParam String name, @RequestParam String description, @RequestParam ArrayList<Product> products)
+    {
+        storefrontRepository.save(new Storefront(name, description, products));
+
+        return "admin/editStorefronts.html";
     }
 
 }
